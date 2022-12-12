02ac51533f75b0cf9a7118ecfe6b1c1ed12105d3
adva-mo <advamozes@gmail.com>
2022-12-12 10:20:30 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   const { isLoading, getData } = useHttp(setData);
 
   useEffect(() => {
-    getData({ url: "/api/bank/all" });
+    getData({ url: "https://bankapi-5xef.onrender.com/api/bank/all" });
     // eslint-disable-next-line
   }, []);
 
