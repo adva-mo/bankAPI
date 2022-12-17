51da0f1d6cd5b4316f41f6ba4261854718a359af
adva-mo <advamozes@gmail.com>
2022-12-17 14:35:59 +0200

    test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,11 @@ dotenv.config();
 
 const URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.nf3dtgt.mongodb.net/?retryWrites=true&w=majority`;
 mongoose.set("strictQuery", false);
-mongoose.connect(URL, (err, mongoDbInstance) => {
-  if (err) throw Error("MongooDb connection Error" + err);
-});
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log("connected to mongoDB");
+  })
+  .catch((e) => {
+    console.log("MongooDb connection Error" + e);
+  });
